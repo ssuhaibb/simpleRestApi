94b1c89b918bc113f5bb773bfc82e3f131ec4b69
ssuhaibb <ssuhaibb@gmail.com>
2019-03-09 14:25:05 +0530

    initial commit

---
+++ customers.js
@@ -0,0 +1,28 @@
+const customers = [
+  {
+    firstname: "Jack",
+    lastname: "Davis",
+    age: 25,
+    id: 1
+  },
+  {
+    firstname: "Mary",
+    lastname: "Taylor",
+    age: 37,
+    id: 2
+  },
+  {
+    firstname: "Peter",
+    lastname: "Thomas",
+    age: 17,
+    id: 3
+  },
+  {
+    firstname: "Peter",
+    lastname: "Thomas",
+    age: 17,
+    id: 4
+  }
+];
+
+module.exports = customers;
